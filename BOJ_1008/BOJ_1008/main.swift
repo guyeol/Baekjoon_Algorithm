3f709bf2af418a18649bff2ae8f8a8e35b0d76be
정구열 <jeong-guyeol@jeong-guyeol-ui-MacBook-2.local>
2018-10-25 17:30:53 +0900

    문제 #4673 셀프넘버 해결

---
+++ BOJ_1008/BOJ_1008/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  BOJ_1008
+//
+//  Created by 정구열 on 2018. 10. 19..
+//  Copyright © 2018년 guyeol_jeong. All rights reserved.
+//
+
+import Foundation
+var a = readLine()
+if let a = a{
+  for i in 1 ..< Int(a)! {
+    print("*")
+  }
+}
